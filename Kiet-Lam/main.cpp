1bb3d089f78c292f998ce2eea130b5807329de06
Mesa Robotics Organization <robomro@gmail.com>
2011-10-21 04:26:12 +0000

    first push for example code

---
+++ Kiet-Lam/main.cpp
@@ -0,0 +1,22 @@
+//_______________________________________________________________________________
+//              
+//                             Author: Kiet Lam
+//                             File main.cpp
+//_______________________________________________________________________________
+// Last Updated: Time-stamp: <2011-10-21 04:25:36 (lam)>
+//
+//
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+
+  cout << "Hello MRO!!!" << endl;
+  
+  return 0;
+}
