6b6ed33f3cf02c13d71e1a51770215ce8504946b
Kiet Lam <ktklam9@gmail.com>
2012-05-27 09:52:22 -0700

    initial commit for android robot

---
+++ AndroidRobot/src/com/mro/android/AndroidRobotActivity.java
@@ -0,0 +1,13 @@
+package com.mro.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidRobotActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
